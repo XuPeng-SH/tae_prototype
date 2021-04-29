c90b343d2fc4a19695fc52bb9a77466c0c095a70
XuPeng-SH <xupeng3112@163.com>
2021-04-29 19:26:31 +0800

    add logic type

---
+++ pkg/common/types/logic_type.go
@@ -0,0 +1,47 @@
+package types
+
+import ()
+
+func NewLogicType(options ...LTOption) *LogicType {
+	lt := &LogicType{
+		LType: INVALID,
+	}
+	for _, option := range options {
+		*lt = option(*lt)
+	}
+	return lt
+}
+
+type LTOption func(LogicType) LogicType
+
+func WithLogicTypeId(id LogicalTypeId) LTOption {
+	return func(lt LogicType) LogicType {
+		lt.LType = id
+		return lt
+	}
+}
+
+func WithWidthAndScale(width, scale uint8) LTOption {
+	return func(lt LogicType) LogicType {
+		lt.Width = width
+		lt.Scale = scale
+		return lt
+	}
+}
+
+func WithCollation(collation string) LTOption {
+	return func(lt LogicType) LogicType {
+		lt.Collation = collation
+		return lt
+	}
+}
+
+func (lt *LogicType) getPhysicalType() PhysicalType {
+	switch lt.LType {
+	case BOOLEAN:
+		return P_BOOL
+	case TINYINT:
+		return P_INT8
+	}
+	return P_INVALID
+}
--- pkg/common/types/types.go
+++ pkg/common/types/types.go
@@ -6,8 +6,57 @@ type IDX_T uint64
 type PhysicalType uint8
 
 const (
-	NA PhysicalType = iota
-	BOOL
-	UINT8
-	UINT16
+	P_NA PhysicalType = iota
+	P_BOOL
+	P_UINT8
+	P_INT8
+	P_UINT16
+	P_INT16
+	P_INVALID
 )
+
+type LogicalTypeId uint8
+
+const (
+	INVALID LogicalTypeId = iota
+	SQLNULL               = 1 /* NULL type, used for constant NULL */
+	UNKNOWN               = 2 /* unknown type, used for parameter expressions */
+	ANY                   = 3 /* ANY type, used for functions that accept any type as parameter */
+
+	BOOLEAN   = 10
+	TINYINT   = 11
+	SMALLINT  = 12
+	INTEGER   = 13
+	BIGINT    = 14
+	DATE      = 15
+	TIME      = 16
+	TIMESTAMP = 17
+	DECIMAL   = 18
+	FLOAT     = 19
+	DOUBLE    = 20
+	CHAR      = 21
+	VARCHAR   = 22
+	BLOB      = 24
+	INTERVAL  = 25
+	UTINYINT  = 26
+	USMALLINT = 27
+	UINTEGER  = 28
+	UBIGINT   = 29
+	HUGEINT   = 50
+	POINTER   = 51
+	HASH      = 52
+	VALIDITY  = 53
+
+	STRUCT = 100
+	LIST   = 101
+	MAP    = 102
+	TABLE  = 103
+)
+
+type LogicType struct {
+	PType     PhysicalType
+	LType     LogicalTypeId
+	Width     uint8
+	Scale     uint8
+	Collation string
+}
