8e46f196d48e45ee84940a6d7ff649519f928312
XuPeng-SH <xupeng3112@163.com>
2021-04-30 12:18:14 +0800

    add value 2

--- pkg/common/types/value/value.go
+++ pkg/common/types/value/value.go
@@ -3,11 +3,11 @@ package value
 import (
 	"encoding/binary"
 	// log "github.com/sirupsen/logrus"
+	"fmt"
 	"math"
 	"tae/pkg/common/types"
 )
 
-// func NewValue(v interface{}) *Value {
 func NewValue(vs ...interface{}) *Value {
 	val := &Value{IsNull: true}
 	if len(vs) == 0 {
@@ -83,13 +83,50 @@ func NewValue(vs ...interface{}) *Value {
 		val.Type = *(types.NewLogicType(types.WithLogicTypeId(types.FLOAT64)))
 		binary.BigEndian.PutUint64(val.Val.Data, math.Float64bits(x))
 		val.IsNull = false
+	default:
+		panic(fmt.Sprintf("UNKNOWN value type %T", x))
 	}
 
 	return val
 }
 
-func (v *Value) GetValue() interface{} {
-	return 1
+func (v *Value) GetValue() (ret interface{}) {
+	if v.IsNull {
+		return ret
+	}
+	switch v.Type.LType {
+	case types.BOOLEAN:
+		if v.Val.Data[0] == 0 {
+			ret = false
+		} else {
+			ret = true
+		}
+	case types.TINYINT:
+		ret = int8(v.Val.Data[0])
+	case types.UTINYINT:
+		ret = uint8(v.Val.Data[0])
+	case types.SMALLINT:
+		ret = (int16)(binary.BigEndian.Uint16(v.Val.Data))
+	case types.USMALLINT:
+		ret = binary.BigEndian.Uint16(v.Val.Data)
+	case types.INTEGER:
+		ret = (int32)(binary.BigEndian.Uint32(v.Val.Data))
+	case types.UINTEGER:
+		ret = binary.BigEndian.Uint32(v.Val.Data)
+	case types.BIGINT:
+		ret = (int64)(binary.BigEndian.Uint64(v.Val.Data))
+	case types.UBIGINT:
+		ret = binary.BigEndian.Uint64(v.Val.Data)
+	case types.FLOAT32:
+		bytes := binary.BigEndian.Uint32(v.Val.Data)
+		ret = math.Float32frombits(bytes)
+	case types.FLOAT64:
+		bytes := binary.BigEndian.Uint64(v.Val.Data)
+		ret = math.Float64frombits(bytes)
+	default:
+		panic(fmt.Sprintf("UNKNOWN value type %v", v.Type.LType))
+	}
+	return ret
 }
 
 // func Min(lt LogicType) Value {
